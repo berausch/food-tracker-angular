217acdea2d6af036b35ceb7ae0221436354d2f43
Brittany Rausch <berausch247@gmail.com>
2016-08-12 13:55:27 -0700

    Create the new food component

--- app/new-food.component.ts
+++ app/new-food.component.ts
@@ -8,10 +8,9 @@ import { Food } from "./food.model";
     <h3>Create New Food</h3>
     <div class='food-form'>
       <h4>Name<input placeholder="Name" class="input-lg task-form" #newFoodName></h4>
-      <h4>Brand<input placeholder="Brand" class="input-lg task-form" #newFoodBrand></h4>
-      <h4>Alcohol Content<input placeholder="AlcoholContent" class="input-lg task-form" #newFoodAlcoholContent><br></h4>
-      <h4>Price<input placeholder="Price" class="input-lg task-form" #newFoodPrice></h4>
-      <h4><button (click)="addFood(newFoodName, newFoodBrand, newFoodAlcoholContent, newFoodPrice)">Submit</button></h4>
+      <h4>Details<input placeholder="Details" class="input-lg task-form" #newFoodDetails></h4>
+      <h4>Calories: <input placeholder="Calories" class="input-lg task-form" #newFoodCalories><br></h4>
+      <h4><button (click)="addFood(newFoodName, newFoodDetails, newFoodCalories)">Submit</button></h4>
     </div>
   `
 })
@@ -21,12 +20,11 @@ export class NewFoodComponent {
   constructor() {
     this.onSubmitNewFood = new EventEmitter();
   }
-  addFood(foodName: HTMLInputElement, foodBrand: HTMLInputElement, alcoholContent: HTMLInputElement, foodPrice: HTMLInputElement) {
-    var newFood: Food = new Food(foodName.value, foodBrand.value, parseInt(alcoholContent.value), parseInt(foodPrice.value));
+  addFood(foodName: HTMLInputElement, foodDetails: HTMLInputElement, foodCalories: HTMLInputElement) {
+    var newFood: Food = new Food(foodName.value, foodDetails.value, parseInt(foodCalories.value));
     this.onSubmitNewFood.emit(newFood);
     foodName.value = "";
-    foodBrand.value = "";
-    alcoholContent.value = "";
-    foodPrice.value = "";
+    foodDetails.value = "";
+    foodCalories.value = "";
   }
 }
