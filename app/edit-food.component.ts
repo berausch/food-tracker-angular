0a50e86e1e23d68cd7f3b7c6b6ad632fc70cdf0b
Brittany Rausch <berausch247@gmail.com>
2016-08-12 13:52:27 -0700

    Create the edit food component

--- app/edit-food.component.ts
+++ app/edit-food.component.ts
@@ -1,23 +1,20 @@
 import { Component } from 'angular2/core';
-import { Keg } from './keg.model';
+import { Food } from './food.model';
 
 
 @Component({
-  selector: 'edit-keg',
-  inputs: ['keg'],
+  selector: 'edit-food',
+  inputs: ['food'],
   template: `
-    <h3>Edit Keg</h3>
-    <div class='keg-object'>
-      <h4 class='keg'>Name<input [(ngModel)]="keg.name" class="input-lg task-form"/></h4>
-      <h4>Brand<input [(ngModel)]="keg.brand" class="input-lg task-form"/></h4>
-      <h4>Alcohol Content<input [(ngModel)]="keg.alcoholContent" class="input-lg task-form"/><br></h4>
-      <br><h4>Pints<input [(ngModel)]="keg.pints" class="input-lg task-form"/></h4>
-      <br>
-      <h4>Price<input [(ngModel)]="keg.price" class="input-lg task-form"/></h4>
+    <h3>Edit Food</h3>
+    <div class='food-object'>
+      <h4 class='food'>Name<input [(ngModel)]="food.name" class="input-lg task-form"/></h4>
+      <h4>Details<input [(ngModel)]="food.details" class="input-lg task-form"/></h4>
+      <h4>Calories<input [(ngModel)]="food.calories" class="input-lg task-form"/><br></h4>
     </div>
   `
 })
 
-export class EditKegComponent {
-  public keg: Keg;
+export class EditFoodComponent {
+  public food: Food;
 }
