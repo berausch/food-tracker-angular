eb3178c41fb2c0acae0e6d27393ba5277c13d5f9
Brittany Rausch <berausch247@gmail.com>
2016-08-12 14:21:06 -0700

    Create the calorie-high-low pipe

---
+++ app/calorieshighlow.pipe.ts
@@ -0,0 +1,32 @@
+import { Pipe, PipeTransform } from 'angular2/core';
+import { Food } from './food.model';
+
+
+@Pipe ({
+  name: "calories-high-low",
+  pure:false
+})
+
+export class CaloriesHighLowPipe implements PipeTransform {
+  tranform(input: Food[], info) {
+    var output: Task[] = [];
+    var highLowCalories = info[0];
+    if(highLowCalories === "high"){
+      for(var i = 0; i<input.length; i++) {
+        if(input[i].calories >= 500) {
+          output.push(input[i]);
+          }
+        }
+        return output;
+    } else if(highLowCalories === "low"){
+      for(var i = 0; i<input.length; i++) {
+        if(input[i].calories < 500) {
+          output.push(input[i]);
+        }
+      }
+      return output;
+    }  else {
+      return input;
+    }
+  }
+}
